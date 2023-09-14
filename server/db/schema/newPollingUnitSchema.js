a3a14afd78ee03ad7fc60ae295f9095d9772906a
Matthew Arofin <Matt.arofin@gmail.com>
2023-09-14 04:00:36 +0100

    updates to schema and seed files. Final testing imminent

--- server/db/schema/newPollingUnitSchema.js
+++ server/db/schema/newPollingUnitSchema.js
@@ -3,7 +3,7 @@ import yup from 'yup';
 // Schema for polling unit result entry validation
 const pollingUnitSchema = yup.object().shape({
   result_id: yup.number().integer(),
-  polling_unit_uniqueid: yup.number().integer().required(),
+  polling_unit_uniqueid: yup.string().trim().required('Polling Unit Unique ID is required'),
   party_abbreviation: yup.string().trim().max(4).required('Party Abbreviation is required'),
   party_score: yup.number().integer().min(0).required('Party Score is required and must be a non-negative integer'),
   entered_by_user: yup.string().trim().required('Entered by User is required'),
