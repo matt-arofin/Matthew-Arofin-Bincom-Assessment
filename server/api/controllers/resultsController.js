077984a8d206a0dec622d219681d8ab9eba5d9bb
Matthew Arofin <Matt.arofin@gmail.com>
2023-09-14 01:06:32 +0100

    Update to API routes

--- server/api/controllers/resultsController.js
+++ server/api/controllers/resultsController.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import resultsModel from '../models/resultsModels';
 
+
+
 const router = express.Router();
 
-// GET results for individual polling units (By PU ID)
-router.get('/', async (req, res) => {
+// GET result for individual polling units (By PU Unique ID)
+// Should return the result of an individual polling unit
+router.get('/:id', async (req, res) => {
   try {
-    const 
+    const results = await resultsModel.query();
+    res.status(200).json(results)
   } catch(error) {
-
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// GET aggregate results of polling units in LGAs (By LGA ID)
-router.get('/', async (req, res) => {
+// GET aggregate results of polling units in LGAs (By LGA Unique ID)
+//
+router.get('/:id', async (req, res) => {
   try {
 
   } catch(error) {
@@ -24,8 +30,10 @@ router.get('/', async (req, res) => {
 // POST new polling unit results including votes for all parties
 router.post('/', async (req, res) => {
   try {
-
+    const newPoll = await resultsModel.query().insert(req.body);
+    res.status(201).json(newPoll);
   } catch(error) {
-    
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
